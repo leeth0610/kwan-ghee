ab6f95045f944ac817269c437cafc6a90ea3ca4c
이태형 <lee@itaehyeong-ui-MacBook-Pro.local>
2017-01-25 18:09:02 +0900

    3

--- Server.js
+++ Server.js
@@ -5,12 +5,18 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', (req, res) => {
-        res.sendFile('./index.html');
+app.get('/index.html', (req, res) => {
+        res.sendFile(__dirname + '/index.html');
 });
 
-app.post('/', (req, res) => {
-        res.send(req.body);
+app.post('/index.html', (req, res) => {
+        const dataArr = req.body.data;
+
+        let str = '하찮다';
+        if (dataArr[0] == 'true')
+                str = '현명하다';
+
+        res.send(str);
 });
 
 app.listen(1818, () => {
